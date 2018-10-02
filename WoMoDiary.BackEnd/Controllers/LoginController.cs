79adb67f0e19f3fb9b21f157e0ffdcabcc60255b
Marcel@Dell <marcel.benders@outlook.de>
2018-10-03 00:58:41 +0200

    controller changes

--- WoMoDiary.BackEnd/Controllers/LoginController.cs
+++ WoMoDiary.BackEnd/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WoMoDiary.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace WoMoDiary.BackEnd.Controllers
 {
@@ -12,13 +14,15 @@ namespace WoMoDiary.BackEnd.Controllers
     public class LoginController : ControllerBase
     {
         private readonly WoMoContext _context;
+        private ILogger<LoginController> _logger;
 
-        public LoginController(WoMoContext context)
+        public LoginController(WoMoContext context, ILogger<LoginController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        // GET api/trip
+        // GET api/user
         [HttpGet]
         public ActionResult<IEnumerable<User>> Get()
             => new OkResult();
@@ -27,8 +31,10 @@ namespace WoMoDiary.BackEnd.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> Get(Guid id)
         {
+            _logger.LogWarning($"Looking for User with GUID '{id.ToString()}'");
             var user = await _context.Users.SingleOrDefaultAsync(i => i.Id == id);
-            if (user == null) return new NotFoundResult();
+            if (user == null) return new NotFoundObjectResult("Error");
+            _logger.LogWarning($"Returns with User '{user}'");
             return user;
         }
 
@@ -43,14 +49,14 @@ namespace WoMoDiary.BackEnd.Controllers
 
         // PUT api/user/1436DD2A-3AE6-44AE-B369-8145E5AD69AD
         [HttpPut("{id}")]
-        public async Task<ActionResult> Put(Guid id, [FromBody] User value)
+        public async Task<ActionResult<User>> Put(Guid id, [FromBody] User value)
         {
             var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == id);
             _context.Users.Remove(user);
             value.LastEdit = DateTimeOffset.Now;
             var newUser = await _context.Users.AddAsync(value);
             var result = await _context.SaveChangesAsync();
-            return new OkObjectResult(newUser);
+            return value;
         }
 
         // DELETE api/user/1436DD2A-3AE6-44AE-B369-8145E5AD69AD
