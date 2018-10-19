8cc8df782d8f7fc94043c6ac060cac4448b6995b
Marcel Benders <mbenders@cgn.cleverbridge.com>
2018-10-19 11:51:08 +0200

    Add Trip Added

--- App.cs
+++ App.cs
@@ -12,13 +12,14 @@ namespace com.b_velop.WoMoDiary
     {
 
 #if DEBUG
-        public static bool UseMockDataStore = false;
+        public static bool UseMockDataStore = true;
 #else
         public static bool UseMockDataStore = false;
 #endif
 
         public static bool Init { get; set; }
         public static bool AllDataFetched { get; set; }
+        // ReSharper disable once InconsistentNaming
         public const string BACKEND_URL = "https://womo.marcelbenders.de";
 
         public App()
