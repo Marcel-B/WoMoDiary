787122f5ddf5c6dad5f8646c900c8cf44a9310b3
marcel@macBook <marcel.benders@icloud.com>
2018-10-21 22:19:02 +0200

    iOS Map BUGFIX

--- WoMoDiary.iOS/ViewController/MyPlacesMapViewController.cs
+++ WoMoDiary.iOS/ViewController/MyPlacesMapViewController.cs
@@ -1,6 +1,11 @@
 using System;
 using UIKit;
 using com.b_velop.WoMoDiary.Meta;
+using com.b_velop.WoMoDiary.Services;
+using com.b_velop.WoMoDiary.Domain;
+using System.Collections.Generic;
+using MapKit;
+using CoreLocation;
 
 namespace com.b_velop.WoMoDiary.iOS
 {
@@ -14,6 +19,30 @@ namespace com.b_velop.WoMoDiary.iOS
             base.ViewDidAppear(animated);
             ItemMap.Title = Strings.MAP;
             NavigationItem.SetHidesBackButton(true, false);
+            SetStates();
+        }
+
+
+        private void SetStates()
+        {
+            var aa = AppStore.Instance.User.Trips;
+            var allPlaces = new List<Place>();
+            foreach (var trip in aa)
+            {
+                allPlaces.AddRange(trip.Places);
+            }
+            MapViewMyPlaces.RemoveAnnotations(MapViewMyPlaces.Annotations);
+            var annotations = new List<MKPointAnnotation>();
+            foreach (var place in allPlaces)
+            {
+                var coordinate = new CLLocationCoordinate2D(place.Latitude, place.Longitude);
+                annotations.Add(new MKPointAnnotation
+                {
+                    Coordinate = coordinate,
+                    Title = place.Name
+                });
+            }
+            MapViewMyPlaces.ShowAnnotations(annotations.ToArray(), true);
         }
     }
 }
